5a8ea5ca30bcaae9b885b3eb3ebe86feee12629c
dreessen-n <dreessen.edu@gmail.com>
2022-06-30 21:43:02 -0600

    Create rename new_user to new.html, write routes

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, render_template, request, redirect, session
+from user import User
 
 app = Flask(__name__)
+app.secret_key='REinstall working well'
 
 @app.route('/')
 def index():
@@ -10,7 +12,24 @@ def index():
 @app.route('/users')
 def show_all():
     """Render All the users"""
-    return render_template('users.html')
+    users = User.get_all()
+    print(users)
+    return render_template('users.html', all_users=users)
+
+@app.route('/users/new', methods=['POST'])
+def create_new_user():
+    """Add a new user to db users"""
+    #  First create a dict based on request form
+    # The keys need to exactly match the variables in query string
+    data = {
+        'first_name': request.form['first_name'],
+        'last_name': request.form['last_name'],
+        'email': request.form['email']
+    }
+    # pass the data dict to the save_user() in User class
+    User.save_user(data)
+    # Do not forget to redirect after saving to db
+    return redirect('/users')
 
 @app.errorhandler(404)
 def page_not_found(e):
